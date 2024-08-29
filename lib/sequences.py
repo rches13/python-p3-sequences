1097428325508699048d5747f5637ed4a53f4e68
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-08-29 08:42:51 +0300

    add sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+       if length == 0:
+        print([])
+        return
+    
+       if length < 0:
+        print("Length should be a positive integer.")
+        return
+    
+       fibonacci_sequence = [0, 1]
+    
+       while len(fibonacci_sequence) < length:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    # Adjust the sequence to the requested length
+       print(fibonacci_sequence[:length])
